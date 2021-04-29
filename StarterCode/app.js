7a5c9c2b6857ab2f59baeb9f985256f6f27d43ce
cdturner618 <cdturner618@gmail.com>
2021-04-29 18:32:43 -0500

    update

--- StarterCode/app.js
+++ StarterCode/app.js
@@ -1,13 +1,23 @@
-d3.json("static\js/samples.json").then((data) => {
+d3.json("static/js/samples.json").then((data) => {
     console.log(data);
 
+    // function unpack(rows, index) {
+    //     return rows.map(function (row) {
+    //         return row[index];
+    //     });
+    // }
+
+
+
     var subjectID = data.names;
-    var sampples = data.samples
-    var otu_ids = data.otu_ids;
-    var otu_labels = data.otu_labels;
+    var samples = data.samples
+    var otu_ids = data.sample;
+    var otu_labels = data.sample;
     console.log(samples)
+    console.log(subjectID)
+    console.log(otu_ids)
+    console.log(otu_labels)
 });
 
-// // Promise Pending
-// const dataPromise = d3.json(data);
-// console.log("Data Promise: ", dataPromise)
+
+
