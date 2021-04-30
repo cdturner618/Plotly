e404eacc11f64bd76a5f3b795fa45d7d98cfd12b
cdturner618 <cdturner618@gmail.com>
2021-04-29 20:58:05 -0500

    working update

--- StarterCode/app.js
+++ StarterCode/app.js
@@ -7,17 +7,53 @@ d3.json("static/js/samples.json").then((data) => {
     //     });
     // }
 
-
-
     var subjectID = data.names;
     var samples = data.samples
-    var otu_ids = data.sample;
-    var otu_labels = data.sample;
+
+
+    // function filterdata(data) {
+    //     return data.samples;
+    // }
+    // var filter = samples.filter(filterdata);
+    // console.log(filter);
+
+    var otu_ids = data.map(sample => sample.otu_ids);
+    // var otu_label = data.sample[1];
+
     console.log(samples)
     console.log(subjectID)
     console.log(otu_ids)
-    console.log(otu_labels)
+    // console.log(otu_labels)
 });
 
+// var trace1 = {
+//     type: "bar",
+//     // mode: "lines",
+//     // name: name,
+//     x: samples,
+//     y: subjectID,
+//     line: {
+//         color: "#17BECF"
+//     }
+// };
+
+// var data = [trace1];
+
+// var layout = {
+//     title: `${stock} closing prices`,
+//     xaxis: {
+//         range: [startDate, endDate],
+//         type: "date"
+//     },
+//     yaxis: {
+//         autorange: true,
+//         type: "linear"
+//     }
+// };
+
+// Plotly.newPlot("plot", data, layout);
+
+//   });
+// }
 
 
